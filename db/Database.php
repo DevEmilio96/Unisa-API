58d1bb4a2e84f25ef6c770b30841fd9f695b25dd
DevEmilio96 <minima998@gmail.com>
2024-02-19 18:47:19 +0100

    db fix

--- db/Database.php
+++ db/Database.php
@@ -5,23 +5,21 @@ class Database {
     private static $instance;
     // Variabile per la connessione al database
     private $connection;
-    // Leggi le configurazioni dal file .ini
-    
-    //$config = parse_ini_file('conf.ini'); // prod
-    $config = parse_ini_file('conf_test.ini'); //test
 
-    // Recupera le configurazioni specifiche del database
-    $host = $config['host'];
-    $username = $config['username'];
-    $password = $config['password'];
-    $dbname = $config['dbname'];
 
     // Metodo privato per impedire la creazione di nuove istanze della classe Database
     private function __construct() {
+        // Leggi le configurazioni dal file .ini
+        $this->config = parse_ini_file('conf_test.ini');
+
+        // Recupera le configurazioni specifiche del database
+        $this->host = $this->config['host'];
+        $this->username = $this->config['username'];
+        $this->password = $this->config['password'];
+        $this->dbname = $this->config['dbname'];
+
         // Configura la connessione al database
-        $connection = new mysqli($host, $username, $password, $dbname);
-        
-        // Gestione degli errori di connessione
+        $this->connection = new mysqli($this->host, $this->username, $this->password, $this->dbname);
         if ($this->connection->connect_error) {
             die("Connessione al database fallita: " . $this->connection->connect_error);
         }
@@ -60,6 +58,49 @@ class Database {
     // Metodo per ottenere l'ID generato automaticamente dall'ultima query INSERT
     public function lastInsertId() {
         return $this->connection->insert_id;
+    }
+    public function insert($table, $data) {
+        $keys = implode(", ", array_keys($data));
+        $values = "'" . implode("', '", $data) . "'";
+        $sql = "INSERT INTO $table ($keys) VALUES ($values)";
+        return $this->query($sql);
+    }
+
+    public function update($table, $data, $conditions) {
+        $set = [];
+        foreach ($data as $key => $value) {
+            $set[] = "$key = '$value'";
+        }
+        $setClause = implode(", ", $set);
+        $where = [];
+        foreach ($conditions as $key => $value) {
+            $where[] = "$key = '$value'";
+        }
+        $whereClause = implode(" AND ", $where);
+        $sql = "UPDATE $table SET $setClause WHERE $whereClause";
+        return $this->query($sql);
+    }
+
+    public function delete($table, $conditions) {
+        $where = [];
+        foreach ($conditions as $key => $value) {
+            $where[] = "$key = '$value'";
+        }
+        $whereClause = implode(" AND ", $where);
+        $sql = "DELETE FROM $table WHERE $whereClause";
+        return $this->query($sql);
+    }
+
+    public function select($table, $conditions = []) {
+        $where = '';
+        if (!empty($conditions)) {
+            $where = " WHERE ";
+            $where .= implode(" AND ", array_map(function ($key, $value) {
+                return "$key = '$value'";
+            }, array_keys($conditions), $conditions));
+        }
+        $sql = "SELECT * FROM $table" . $where;
+        return $this->query($sql);
     }
 }
 
