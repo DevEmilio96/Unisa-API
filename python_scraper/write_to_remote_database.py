1da1f26ccc3aae98fa35ad89bc4ffc8c95c35714
DevEmilio96 <minima998@gmail.com>
2024-02-21 17:06:54 +0100

    write on db

--- python_scraper/write_to_remote_database.py
+++ python_scraper/write_to_remote_database.py
@@ -15,13 +15,37 @@ def write_to_remote_database(professors_data):
             connection.begin()
             
             for id, prof in enumerate(professors_data, start=1):
-                # Esecuzione della query per scrivere i dati del professore nel database
+                # teachers
                 sql_professor = "INSERT INTO teachers (id, name, title, department, email, office, personal_webpage, url, image_url) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s)"
                 values_professor = (id, prof['nome'], prof['titolo'], prof['dipartimento'], prof['email'], prof['ufficio'], prof['pagina_personale'], prof['url'], prof['url_immagine_professore'])
                 cursor.execute(sql_professor, values_professor)
+
+                # phone_contacts
+                if isinstance(prof['telefono'], str):
+                    if prof['telefono'] == "Null" : continue
+                    sql_course = "INSERT INTO phone_contacts (id_professore, tel) VALUES (%s, %s)"
+                    values_course = (id, prof['telefono'])
+                    cursor.execute(sql_course, values_course)
+
+                if isinstance(prof['telefono'], list):
+                    for telefono in prof['telefono']:
+                        sql_course = "INSERT INTO phone_contacts (id_professore, tel) VALUES (%s, %s)"
+                        values_course = (id, telefono)
+                        cursor.execute(sql_course, values_course)
                 
-                # Esecuzione della query per scrivere i dati dei corsi del professore nel database
+                # reception_hours
+                for orario in prof['orari_di_ricevimento']:
+                # Controllo sulla lunghezza della tupla
+                    if orario["day"]!= "Null":
+                        giorno = orario["day"]
+                        timings = orario["timings"]
+                        luogo = orario["location"]
+                        
+                        sql_course = "INSERT INTO reception_hours (id_professore, day, hours, location) VALUES (%s, %s, %s, %s)"
+                        values_course = (id, giorno, timings, luogo)
+                        cursor.execute(sql_course, values_course)
                 
+                # courses
                 for corso in prof['corsi']:
                     sql_course = "INSERT INTO courses (id_professore, name, code, department, degree) VALUES (%s, %s, %s, %s, %s)"
                     values_course = (id, corso["name"], corso["code"], corso["department"], corso["degree"])
