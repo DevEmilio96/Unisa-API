f7db2f60a68373a5afa753dcbd9767882536e236
DevEmilio96 <minima998@gmail.com>
2024-02-24 21:00:44 +0100

    blackbox and whitebox

--- python_scraper/chatbot_utils/formatters/TextResponseFormatter.py
+++ python_scraper/chatbot_utils/formatters/TextResponseFormatter.py
@@ -3,26 +3,26 @@ from chatbot_utils.utils import *
 class TextResponseFormatter:
     ########################################### # domande sui professori ###########################################
     def default(data):
-        return {"data" : data, "type" : "professore"}
+        return {"data" : data, "type" : {"mode" : "professore"}}
     
     ########################################### # domande sui dipartimenti ###########################################
     def format_dipartimento_campo(self, department_or_field, professori, keyword=None):
         matched_professors = find_professors_by_department_or_field(
             department_or_field, professori
         )
-        return {"data" : matched_professors, "type" : ["professori-per-dipartimento",department_or_field]}
+        return {"data" : matched_professors, "type" : {"mode" : "professori-per-dipartimento", "value" : department_or_field}}
     
     
     def format_offerta_formativa_dipartimento(self,domanda, dipartimenti, keyword=None):
         dipartimento = find_department_by_department_name(domanda, dipartimenti)
         if dipartimento:
-            return {"data" : dipartimento, "type" : "dipartimento"}
+            return {"data" : dipartimento, "type" : {"mode" : "dipartimento"}}
     
     ########################################### # domande sui corsi ###########################################
     def format_insegnamento(self, domanda, professori, keyword=None):
         course_name = extract_course_name(domanda)
         professors_for_course = find_professors_for_course(course_name, professori)
-        return {"data" : professors_for_course, "type" : ["professori-per-corso",course_name]}
+        return {"data" : professors_for_course, "type" : {"mode" : "professori-per-corso", "value" : course_name}}
        
     def format_offerta_formativa_corso(self, domanda, professori, keyword):
         course_name = extract_course_name(domanda)
@@ -45,7 +45,7 @@ class TextResponseFormatter:
         # Verifica se il corso è stato trovato e se ha una 'scheda'
         if dettagli_corso_cercato and 'scheda' in dettagli_corso_cercato:
             # Se il corso è stato trovato e ha una 'scheda', restituisci i dettagli del corso
-            return {"data" : dettagli_corso_cercato, "type" : "corso"}
+            return {"data" : dettagli_corso_cercato, "type" : {"mode" : "corso"}}
         else:
             # Se il corso non è stato trovato o non ha una 'scheda', restituisci None
             return None
